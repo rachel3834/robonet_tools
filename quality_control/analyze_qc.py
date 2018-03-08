6ead90db8d17dcf85f0ecd66ab75aac0b3f92b02
rachel3834 <rstreet@lco.global>
2018-03-08 13:00:00 -0800

    Adding analysis of lunar separation by filter

--- quality_control/analyze_qc.py
+++ quality_control/analyze_qc.py
@@ -52,6 +52,11 @@ def analyze_qc_data():
     'Measured sky brightness as a function of lunar separation',
     bad_images=bad_images)
     
+    gather_header_qc_data.plot_stats_by_filter(image_list,image_data,dir_path,'moon_distance.png', 12, 
+    'Moon separation [$^{\circ}$]',
+    'Lunar separation',
+    bad_images=bad_images)
+    
     gather_header_qc_data.plot_stats_per_filter(image_list,image_data,dir_path,'mean_sky.png', 17, 
     'Mean sky value [ADU]', 'Average sky background data',
     bad_images=bad_images)
