8e64dd8915647f4959b5fbf3f262e053758e262c
rachel3834 <rstreet@lco.global>
2019-01-03 16:47:02 -0800

    Added file for code to walk the LCO local data archive and harvest data obtained prior to 2014 and the Cloud-based online archive

---
+++ data_harvest/archive_walker.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  3 16:42:56 2019
+
+@author: rstreet
+"""
+
+def search_lco_local_archive():
+    """Function to search the LCO local data archive, primarily for data
+    taken prior to 2014 that is not available through the online Cloud archive
+    service
+    """
+    
+    /net/archive/archive/engineering/site-code/camera/day-date/
+    -> cat/ preview/ processed/ raw/ wcs/
+    -> -e91 frames are in processed, fits.fz compressed
+    
+    
