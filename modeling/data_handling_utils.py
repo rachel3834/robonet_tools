4dda8cad104f3ced7b50fd6632957dc03e7ec182
rachel3834 <rstreet@lco.global>
2019-01-26 12:17:05 -0800

    Re-vamped code for OO approach, drafting code for model handling

--- modeling/data_handling_utils.py
+++ modeling/data_handling_utils.py
@@ -7,97 +7,91 @@ Created on Fri Jan 25 21:44:01 2019
 
 import numpy as np
 import os
+from pyLIMA import telescopes
 
-def apply_error_rescaling(rescaling_coeffs,name,lightcurve,use_error_rescaling):
+class Dataset():
     
-    if len(rescaling) > 0:
+    def __init__(self, params=None):
+        self.name = None
+        self.filter = None
+        self.data_file = None
+        self.tel = None
+        self.gamma = None
+        self.k = None
+        self.k_err = None
+        self.emin = None
+        self.emin_err = None
+        self.a0 = None
+        self.a0_err = None
+        self.a1 = None
+        self.a1_err = None
         
-        lightcurve[:,2] =  (rescaling[0]**2 + rescaling[1]**2*lightcurve[:,2]**2)**0.5
-        
-        print(' -> Applied error rescaling coefficient for dataset '+name+\
-                ' a0='+str(rescaling[0])+', a1='+str(rescaling[1]))
-    return lightcurve
+        if params != None:
+            self.name = params['name']
+            self.filter = params['filter']
+            self.data_file = params['data_file']
+    
+    def summary(self):
+        if self.gamma != None:
+            return self.name+' '+self.filter+' '+self.data_file+' '+str(self.gamma)
+        else:
+            return self.name+' '+self.filter+' '+self.data_file
 
-def read_data_file(data_file,calibrated=False, gamma=None, rescaling=[]):
-    """Function to decide which function to use to read the datafile"""
-    
-    if 'p.t' in data_file:
+    def read_dataset_to_telescope(self,model_type,rescaling=[]):
         
-        tel = read_rbn_lightcurve(data_file, calibrated=calibrated, 
-                                  gamma=gamma, rescaling=rescaling)
+        if 'p.t' in self.data_file:
+            lightcurve = np.loadtxt(self.data_file,dtype=str)
+            lightcurve = np.c_[lightcurve[:,1],lightcurve[:,6],lightcurve[:,7]].astype(float)
         
-    elif 'cal.t' in data_file:
+        if 'cal.t' in self.data_file:
+            lightcurve = np.loadtxt(self.data_file,dtype=str)
+            lightcurve = np.c_[lightcurve[:,1],lightcurve[:,8],lightcurve[:,9]].astype(float)
+                
+        if 'DK-1.54' in self.data_file:
+            lightcurve = np.loadtxt(self.data_file,dtype=str)
+            lightcurve = np.c_[lightcurve[:,1],lightcurve[:,6],lightcurve[:,7]].astype(float)
         
-        tel = read_rbn_lightcurve(data_file, calibrated=calibrated, 
-                                  gamma=gamma, rescaling=rescaling)
+        if len(rescaling) > 0:
+            lightcurve = self.apply_error_rescaling(rescaling,lightcurve)
         
-    elif 'DK-1.54' in data_file:
+        self.tel = telescopes.Telescope(name=self.name, camera_filter=self.filter, 
+                                 light_curve_magnitude=lightcurve,
+                                 light_curve_magnitude_dictionnary={'time': 0, 'mag': 1, 'err_mag': 2})
+        if 'FS' in model_type:
+            self.tel.gamma = self.gamma
+            print(self.tel.name, self.tel.gamma)
+    
+    def fetch_gamma(self):
+
+        gamma_coeffs = { 'gp': 0.8371,
+                         'rp': 0.6445,
+                         'ip': 0.503,
+                         'Z': 0.4134 }
+    
+        gamma = None
+    
+        for f in gamma_coeffs.keys():
+    
+            if '_'+f in self.data_file:
+
+                gamma = gamma_coeffs[f]
+                
+                print(data_file+' -> filter = '+f+' gamma = '+str(gamma))
+    
+        if gamma == None:
+            print('ERROR: No gamma value available for lightcurve '+data_file)
+            exit()
+    
+        return gamma
+
+
+    def apply_error_rescaling(self,coefficients,lightcurve):
         
-        tel = read_danish_lightcurve(data_file, gamma=gamma, rescaling=rescaling)
-    
-    return tel
-    
-def read_rbn_lightcurve(data_file, calibrated=False, gamma=None, rescaling=[]):
-    """Function to read the standard DanDIA-format RoboNet lightcurve file.
-    The calibrated option controls whether the (default) raw, instrumental 
-    magnitudes are read or the calibrated magnitude columns
-    Returns a pyLIMA telescope option.
-    """
-    
-    if calibrated:
-        mag_col = 8
-        mag_err_col = 9
-    else:
-        mag_col = 6
-        mag_err_col = 7
-        
-    lightcurve = np.loadtxt(data_file,dtype=str)
-    lightcurve = np.c_[lightcurve[:,1],lightcurve[:,mag_col],
-                       lightcurve[:,mag_err_col]].astype(float)
-
-    print('\nRead RoboNet lightcurve '+os.path.basename(data_file))
-    
-    event_telescope = os.path.basename(data_file)
-    
-    aaa = event_telescope
-    name = aaa[14:17]+aaa[21]+'_'+aaa[-4:-2]
-    
-    lightcurve = apply_error_rescaling(rescaling,name,lightcurve)
-
-    telescope = telescopes.Telescope(name=name, camera_filter=aaa[-4:-2],
-                     light_curve_magnitude=lightcurve,
-                     light_curve_magnitude_dictionnary={'time': 0, 'mag': 1, 'err_mag': 2})
-
-    if gamma != None:
-        gamma = fetch_gamma(os.path.basename(event_telescope))
-        telescope.gamma = gamma
-        print(' -> Applied gamma='+str(gamma))
-
-    return telescope
-    
-def read_danish_lightcurve(data_file, gamma=None, rescaling=[]):
-    """Function to read the standard MiNDSTEp-format Danish lightcurve file,
-    from the DanDIA pipeline adapted for EMCCDs.
-    Returns a pyLIMA telescope option.
-    """
-
-    lightcurve = np.loadtxt(data_file,dtype=str)
-    lightcurve = np.c_[lightcurve[:,1],lightcurve[:,6],lightcurve[:,7]].astype(float)
-    
-    print('\nRead Danish lightcurve '+os.path.basename(data_file))
-    
-    name = 'Danish'
-    
-    lightcurve = apply_error_rescaling(rescaling,name,lightcurve)
-    
-    telescope = telescopes.Telescope(name=name, camera_filter='Z',
-                                        light_curve_magnitude=lightcurve,
-                                        light_curve_magnitude_dictionnary={'time': 0, 'mag': 1, 'err_mag': 2})
-
-    if gamma != None:
-        gamma = fetch_gamma(os.path.basename(event_telescope))
-        telescope.gamma = gamma
-        print(' -> Applied gamma='+str(gamma))
-    
-    return telescope
-    
+        if len(coefficients) > 0:
+            
+            lightcurve[:,2] =  (coefficients[0]**2 + coefficients[1]**2*lightcurve[:,2]**2)**0.5
+            
+            print(' -> Applied error rescaling coefficient for dataset '+self.name+\
+                    ' a0='+str(coefficients[0])+', a1='+str(coefficients[1]))
+            
+        return lightcurve
--- modeling/pylima_lightcurve_tools.py
+++ modeling/pylima_lightcurve_tools.py
@@ -12,30 +12,33 @@ import copy
 from pyLIMA import microltoolbox
 import matplotlib.pyplot as plt
 import data_handling_utils
+from pyLIMA import event
+from pyLIMA import microlfits
+from pyLIMA import microlmodels
 
 def plot_event_lightcurve():
     """Function to plot an event lightcurve with a model overplotted from
     pyLIMA"""
     
     params = get_params()
-
-    current_event = generate_event(params)
     
-    for i,data_file in enumerate(params['datasets']):
+    rescaling_coeffs = read_error_rescaling_factors(params['error_scaling_file'])
+    
+    params = read_data_files(params, rescaling_coeffs)
+    
+    current_event = create_event(params)
+    
+    for d in params['data']:
         
-        gamma = fetch_gamma(os.path.basename(data_file))
-        
-        rescaling = fetch_rescaling_factors(data_file,params)
-        
-        tel = data_handling_utils.read_data_file(data_file,calibrated=False, 
-                                                 gamma=gamma, rescaling=rescaling)
-        
-        current_event.telescopes.append(telescope)
+        current_event.telescopes.append(d.tel)
     
     current_event.find_survey(params['survey'])
     current_event.check_event()
 
-
+    (current_event,params) = create_model(current_event,params)
+    
+    residual_lcs = generate_residual_lcs(current_event,params)
+    
 def get_params():
     
     params = {}
@@ -56,31 +59,71 @@ def get_params():
     flines = open(input_file,'r').readlines()
     
     datasets = []
+    phot_params = {}
     
     for line in flines:
-        (key,value) = line.replace('\n','').split()
-        
+        key = line.replace('\n','').split()[0]
         key = str(key).lower()
         
         if 'dataset' in key:
-            datasets.append(value)
+            entries = line.replace('DATASET','').replace('\n','').split()
+            ddict = {'name': entries[0], 'filter': entries[1], 
+                     'data_file': entries[2], 'gamma': float(entries[3])}
+            datasets.append(ddict)
         
-        if key in ['output','error_scaling_file','survey']:
-            params[str(key).lower()] = value
+        if key in ['name','output','error_scaling_file','survey','model_type']:
+            entries = line.replace(key.upper(),'').replace('\n','').split()
+            params[key] = entries[0]
         
-        if key in ['ra', 'dec']:
-            params[str(key).lower()] = float(value)
+        if key in ['ra', 'dec', 'to', 'uo', 'te', 'rho', 'logs', 'logq', \
+                    'alpha', 'pien', 'piee', 'dsdt', 'dalphadt']:
+            entries = line.replace(key.upper(),'').replace('\n','').split()
+            params[key] = float(entries[0])
+        
+        if 'fs_' in key or 'fb_' in key:
+            
+            name = key.replace('fs_','').replace('fb_','')
+            value = float(line.replace('\n','').split()[-1])
+            
+            if name in phot_params.keys():
+                p = phot_params[name]
+            else:
+                p = {}
+            
+            if 'fs_' in key:
+                p['fs'] = value
+            else:
+                p['fb'] = value
+            
+            phot_params[name] = p
             
     params['datasets'] = datasets
+    params['phot_params'] = phot_params
     
-    if 'error_scaling_file' in params.keys():
+    return params
 
-        rescaling_coeffs = read_error_rescaling_factors(params['error_scaling_file'])
+def read_data_files(params, rescaling_coeffs):
+    
+    datasets = []
+    
+    for ddict in params['datasets']:
         
-        for key, value in rescaling_coeffs.items():
-            
-            params[key] = value
-            
+        d = data_handling_utils.Dataset(ddict)
+        
+        if 'gamma' in ddict.keys():
+            d.gamma = ddict['gamma']
+        
+        rescale_factors = fetch_rescaling_factors(d.name,rescaling_coeffs)
+        
+        d.read_dataset_to_telescope(params['model_type'], 
+                                    rescaling=rescale_factors)
+        
+        datasets.append( d )
+        
+        print(d.summary())
+        
+    params['data'] = datasets
+    
     return params
     
 def read_error_rescaling_factors(coeffs_file):
@@ -107,42 +150,16 @@ def read_error_rescaling_factors(coeffs_file):
         
     return rescaling_coeffs
 
-def fetch_rescaling_factors(data_file,params):
+def fetch_rescaling_factors(name,rescaling_coeffs):
     
-    if 'DK-1.54' in data_file:
-        name = 'Danish'
-    else:
-        name = data_file[14:17]+data_file[21]+'_'+data_file[-4:-2]
-    
-    if name in params.keys():
-        factors = params['name']
+    if name in rescaling_coeffs.keys():
+        factors = rescaling_coeffs[name]
     else:
         factors = []
-        
+
     return factors
         
-def fetch_gamma(lightcurve_file):
-
-    gamma_coeffs = { 'gp': 0.8371,
-                     'rp': 0.6445,
-                     'ip': 0.503,
-                     'Z': 0.4134 }
-
-    gamma = None
-
-    for f in gamma_coeffs.keys():
-
-        if '_'+f in lightcurve_file:
-            gamma = gamma_coeffs[f]
-            #print(lightcurve_file+' -> filter = '+f+' gamma = '+str(gamma))
-
-    if gamma == None:
-        print('ERROR: No gamma value available for lightcurve '+lightcurve_file)
-        exit()
-
-    return gamma
-
-def generate_event(params):
+def create_event(params):
     """Function to initialise a pyLIMA event object"""
     
     current_event = event.Event()
@@ -152,7 +169,124 @@ def generate_event(params):
     current_event.dec = params['dec']
     
     return current_event
+
+def create_model(current_event,params,diagnostics=False):
     
+    f = microlfits.MLFits(current_event)
+    
+    parallax_params = ['None', params['to']]
+    orbital_params = ['None', params['to']]
+    model_params = ['to', 'uo', 'tE']
+    
+    if params['rho'] != None:
+        
+        model_params.append('rho')
+    
+    if params['logs'] != None and params['logq'] != None and params['alpha'] != None:
+        
+        model_params.append('logs')
+        model_params.append('logq')
+        model_params.append('alpha')
+        
+    if params['pien'] != None and params['piee'] != None:
+        
+        parallax_params = ['Full', params['to']]
+        model_params.append('piEN')
+        model_params.append('piEE')
+
+    if 'sdsdt' in params.keys() and 'dalphadt' in params.keys() and 'sdszdt' in params.keys():
+        
+        orbital_params = ['3D', params['to']]
+        model_params.append('sdsdt')
+        model_params.append('dalphadt')
+        model_params.append('sdszdt')
+    
+    elif 'dsdt' in params.keys() and 'dalphadt' in params.keys():
+        
+        orbital_params = ['2D', params['to']]
+        model_params.append('dsdt')
+        model_params.append('dalphadt')
+        
+    params['model_params'] = model_params
+    
+    model = microlmodels.create_model(params['model_type'], current_event,
+                                  parallax=parallax_params,
+                                  orbital_motion=orbital_params,
+                                  blend_flux_ratio = False)
+    
+    if 'binary origin' in params.keys():
+        model.binary_origin  = params['binary_origin']
+        
+    model.define_model_parameters()
+    
+    f.model = model
+    
+    results = []
+    for key in model_params:
+        results.append(params[str(key).lower()])
+    
+    params['fitted_model_params'] = results
+    
+    results = results + params['phot_params']
+    results.append(params['chisq'])
+    
+    f.fit_results = results
+    
+    current_event.fits.append(f)
+    
+    if diagnostics:
+        fig = microloutputs.LM_plot_lightcurves(f)
+    
+        plt.show()
+        
+        plt.close()
+    
+    return current_event,params
+
+def generate_residual_lcs(current_event,params):
+    
+    f = e.fits[-1]
+    model = f.model
+    
+    pylima_params = model.compute_pyLIMA_parameters(params['fitted_model_params'])
+    
+    residual_lcs = []
+    
+    for tel in current_event.telescopes:
+        
+        flux = tel.lightcurve_flux[:, 1]
+        flux_model = model.compute_the_microlensing_model(tel, pylima_params)[0]
+        
+        dmag = 2.5 * np.log10(flux_model/flux)
+        
+        res_lc = np.copy(tel.lightcurve_magnitude)
+        res_lc[:,1] = dmag
+        
+        residual_lcs.append(res_lc)
+    
+    return residual_lcs
+    
+def plot_lcs(lc_data):
+
+    fig = plt.figure(1,(10,10))
+    
+    for i,lc in enumerate(lc_data):
+        plt.errorbar(lc[:,0],lc[:,1],yerr=lc[:,2],fmt='.')
+    
+    plt.xlabel('HJD')
+    plt.ylabel('Magnitude')
+    
+    rev_yaxis = True
+    if rev_yaxis:
+        [xmin,xmax,ymin,ymax] = plt.axis()
+        plt.axis([xmin,xmax,ymax,ymin])
+    
+    plt.grid()
+    plt.savefig('lc_test.png')
+    
+    plt.close(1)
+
+ 
 def generate_model_lightcurve(e,ts=None,diagnostics=False):
     """Function to produce a model lightcurve based on a parameter set
     fitted by pyLIMA
