b395ba89e693a657ed314d88aaf67bab1114d1b4
rachel3834 <rstreet@lco.global>
2020-04-10 11:37:52 -0700

    Added log file

--- backups/calc_checksums.py
+++ backups/calc_checksums.py
@@ -4,37 +4,43 @@ import subprocess
 import glob
 
 
-def checksum_phot_products(red_dir):
+def checksum_phot_products(red_dir, log_path):
 
     if path.isdir(red_dir) == False:
         raise IOError('Cannot find input photometry directory')
+
+    log = open(log_path, 'w')
 
     data_products = glob.glob(path.join(red_dir, '*'))
 
     for product in data_products:
         if path.isfile(product):
-            calc_checksum(product)
+            calc_checksum(product, log)
         else:
             dataset_products = glob.glob(path.join(product, '*'))
 
             for dproduct in dataset_products:
-                calc_checksum(dproduct)
+                calc_checksum(dproduct, log)
 
-def calc_checksum(file_path):
+    log.close()
+
+def calc_checksum(file_path, log):
 
     cl = ['md5sum', file_path]
 
     p = subprocess.Popen(cl)
     stdoutput = p.communicate()[0]
 
-    if stdoutput:
-        print(stdoutput)
+    print(stdoutput)
+    log.write(stdoutput+'\n')
 
 if __name__ == '__main__':
 
     if len(argv) == 1:
         red_dir = input('Please enter the path to the top-level photometry archive directory: ')
+        log_path = input('Please enter the path to the log file: ')
     else:
         red_dir = argv[1]
+        log_path = argv[2]
 
-    checksum_phot_products(red_dir)
+    checksum_phot_products(red_dir, log_path)
