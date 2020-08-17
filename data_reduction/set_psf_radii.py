98df8d6658fb5a60f44cea9056652d4ec4033871
rachel3834 <rstreet@lco.global>
2020-08-17 16:01:19 -0700

    Added tool to reset psf_dimensions table

---
+++ data_reduction/set_psf_radii.py
@@ -0,0 +1,28 @@
+from os import path
+from sys import argv
+from pyDANDIA import metadata
+import numpy as np
+
+def set_psf_dimensions():
+
+    params = get_args()
+
+    reduction_metadata = metadata.MetaData()
+    reduction_metadata.load_all_metadata(metadata_directory=params['red_dir'],
+                                         metadata_name='pyDANDIA_metadata.fits')
+
+    reduction_metadata.calc_psf_radii()
+
+    reduction_metadata.save_updated_metadata(params['red_dir'],'pyDANDIA_metadata.fits')
+
+def get_args():
+
+    params = {}
+    if len(argv) == 1:
+        params['red_dir'] = input('Please enter the path to the reduction directory: ')
+    else:
+        params['red_dir'] = argv[1]
+
+    return params
+
+if __name__ == '__main__':
