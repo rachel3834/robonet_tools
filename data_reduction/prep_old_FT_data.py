0c6c84e5995b5d23dd71f357fe137c9c5dc988fa
rachel3834 <rstreet@lco.global>
2021-11-04 10:57:04 -0700

    Tools for handling older FT data

---
+++ data_reduction/prep_old_FT_data.py
@@ -0,0 +1,31 @@
+from astropy.io import fits
+import numpy as np
+import glob
+from os import path
+import copy
+
+def update_image_structure(data_dir):
+
+    image_list = glob.glob(path.join(data_dir,'*.fits'))
+
+    for image in image_list:
+        hdu = fits.open(image)
+        new_image = image.replace('.fits','p.fits')
+
+        if len(hdu) == 1 and hdu[0].name == 'PRIMARY':
+            sci = copy.copy(hdu[0])
+            sci.name = 'SCI'
+            bpm = np.zeros(sci.data.shape)
+            new_hdu = hdu = fits.HDUList([sci,bpm])
+            new_hdu.writeto(new_image, overwrite=True)
+            print('Restructured '+new_image)
+            
+
+if __name__ == '__main__':
+
+    if len(argv) > 1:
+        data_dir = argv[1]
+    else:
+        data_dir = input('Please enter the path to data directory: ')
+
+    update_image_structure(data_dir)
