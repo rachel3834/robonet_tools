7dda65ae4646399ccff2201f255a90a6cf950203
rachel3834 <rstreet@lco.global>
2023-01-15 13:59:44 -0800

    Added tool to count number of targets

---
+++ data_harvest/count_events.py
@@ -0,0 +1,35 @@
+from os import path
+import glob
+
+def count_event_datasets(top_dir):
+    """The purpose of this function is to review the datasets gathered on
+    all microlensing observations and count the number of events observed"""
+
+    dir_list = glob.glob(path.join(top_dir, '*'))
+
+    datasets = {}
+    for item in dir_list:
+        if '.txt' not in item:
+            entries = str(path.basename(item)).split('-')
+            target = entries[0]
+            dataid = str(path.basename(item)).replace(target+'_','')
+            data_dir = path.join(item, 'data')
+            if path.isdir(data_dir):
+                data_list = glob.glob(path.join(item, 'data', '*.fits'))
+
+                if target in datasets.keys():
+                    data = datasets[target]
+                    data[dataid] = len(data_list)
+                else:
+                    data = {dataid: len(data_list)}
+                datasets[target] = data
+
+    print('Number of targets: '+str(len(datasets)))
+
+
+if __name__ == '__main__':
+    if len(argv) > 1:
+        top_dir = argv[1]
+    else:
+        top_dir = input('Please enter the path to the top level directory: ')
+    count_event_datasets(top_dir)
