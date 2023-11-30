fb70d250084b7cb928afb612598e8451e56fad41
rachel3834 <rstreet@lcogt.net>
2023-11-29 17:17:24 -0800

    Experiemented with various plot layouts, and finalized one matching the Aladdin projection:

--- data_harvest/plot_survey_footprint_map.py
+++ data_harvest/plot_survey_footprint_map.py
@@ -7,68 +7,145 @@ from astropy.coordinates import Galactic, TETE, SkyCoord
 import numpy as np
 from pyDANDIA import plot_all_fields
 import matplotlib.pyplot as plt
+from matplotlib.patheffects import withStroke
+from PIL import Image
 
 # Definition of the healpixel resolution to use, which should correspond to a resolution of
 # about 1deg
-NSIDE=256
+NSIDE=512
 RESOLUTION = hp.nside2resol(NSIDE, arcmin=True) / 60.0 # degrees
 print('Working with resolution ' + str(RESOLUTION) + 'deg')
 BULGE_REGION = {'ra_range': [260.0, 275.0],
                 'dec_range': [-35.0, -25.0]}
 
-# TRILEGAL data maps accessed from Rubin archive:
+# TRILEGAL data maps accessed from Rubin-sim archive:
 # https://s3df.slac.stanford.edu/data/rubin/sim-data/
+# The highest resolution available has NSIDE=1024, but this map does not have data covering the whole sky and omits
+# the Galactic Plane.  So using the NSIDE=512 map as the best available.
+
+# Background maps
+BACKGROUND = {'DECam':[ 271.014958, -28.75124167,'18:04:03.59', '-28:45:04.47', 'decam_bulge_image.tiff', 0.263],
+              'DSS': [269.335, -29.178, '17:57:20.4', '-29:10:40.8', 'DSS_ROME_footprint_car.png', 6.95],
+              'Aladdin': [269.78358, -29.639406, '17:59:08.06', '-29:38:21.86', 'aladdin_rome_footprint.png', 99.999]}
+
+
 
 def plot_footprint(args):
 
     # Load the data on the density of stars as a function of sky location based on
     # the TRILEGAL galactic model
-    (ahp, hp_log_star_density) = create_star_density_map(args)
+    #(ahp, hp_log_star_density) = create_star_density_map(args)
+
+    # hp.mollview(hp_log_star_density, title="Stellar density",
+    #            min=0.0, max=hp_log_star_density.max())
+    # hp.graticule()
+    # plt.tight_layout()
+    # plt.savefig(path.join(args.output_dir, 'stellar_density_map.png'))
+    # plt.close(3)
 
     # Extract from the density map the healpixels for the Bulge region as a 2D image array
-    bulge_star_density = []
-    for ra in np.arange(BULGE_REGION['ra_range'][0], BULGE_REGION['ra_range'][1], RESOLUTION):
-        row = []
-        for dec in np.arange(BULGE_REGION['dec_range'][0], BULGE_REGION['dec_range'][1], RESOLUTION):
-            pixels = calc_hp_healpixels_for_coords(ra, dec, RESOLUTION, NSIDE)
-            row.append(np.median(hp_log_star_density[pixels]))
-        bulge_star_density.append(row)
-    bulge_star_density = np.array(bulge_star_density)
+    plot_trilegal = False
+    if plot_trilegal:
+    # Load the data on the density of stars as a function of sky location based on
+    # the TRILEGAL galactic model
+        (ahp, hp_log_star_density) = create_star_density_map(args)
+        bulge_star_density = []
+        for ra in np.arange(BULGE_REGION['ra_range'][0], BULGE_REGION['ra_range'][1], RESOLUTION):
+            row = []
+            for dec in np.arange(BULGE_REGION['dec_range'][0], BULGE_REGION['dec_range'][1], RESOLUTION):
+                pixels = calc_hp_healpixels_for_coords(ra, dec, RESOLUTION, NSIDE)
+                row.append(np.median(hp_log_star_density[pixels]))
+            bulge_star_density.append(row)
+        bulge_star_density = np.array(bulge_star_density)
+
+    # Plot a background image
+    plot_bkgd = False
+    if plot_bkgd:
+        bkgd_data = BACKGROUND['Aladdin']
+        BKGD_WIDTH = 4.284
+        BKGD_HEIGHT = 3.054
+        bkgd_image = np.array(Image.open(path.join(args.output_dir, bkgd_data[4])))
+        #bkgd_image = np.fliplr(bkgd_image)
+        #bkgd_image = np.flipud(bkgd_image)
+        dra = (BKGD_WIDTH * 3600.0) / bkgd_image.shape[1]
+        x_center = bkgd_image.shape[1]/2.0
+        ddec = (BKGD_HEIGHT * 3600.0) / bkgd_image.shape[0]
+        y_center = bkgd_image.shape[0]/2.0
+        BKGD_HALF_WIDTH = BKGD_WIDTH / 2.0
+        BKGD_HALF_HEIGHT = BKGD_HEIGHT / 2.0
+        extent = [bkgd_data[0] - BKGD_HALF_WIDTH,
+                  bkgd_data[0] + BKGD_HALF_WIDTH,
+                  bkgd_data[1] - BKGD_HALF_HEIGHT,
+                  bkgd_data[1] + BKGD_HALF_HEIGHT]
 
     # Create a WCS header for the center of the Bulge region
-    w = create_bulge_wcs(BULGE_REGION, RESOLUTION, bulge_star_density)
+    #w = create_wcs(bkgd_data[0], bkgd_data[1], dra, ddec, x_center, y_center)
 
-    plt.subplot(projection=w)
-    plt.imshow(bulge_star_density)
-    plt.grid(color='white', ls='solid')
-    plt.xlabel('RA')
-    plt.ylabel('Dec')
-    plt.show()
+    fig = plt.figure(1, (39, 27))
+    ax = plt.subplot()
 
-    #hp.mollview(hp_log_star_density, title="Stellar density",
-    #            min=0.0, max=hp_log_star_density.max())
-    #hp.graticule()
-    #plt.tight_layout()
-    #plt.savefig(path.join(args.output_dir, 'stellar_density_map.png'))
-    #plt.close(3)
+    if plot_bkgd:
+        plt.imshow(bkgd_image, extent=extent)
 
-def create_bulge_wcs(BULGE_REGION, RESOLUTION, image_data):
+    plot_rome = True
+    if plot_rome:
+        for field_id,field_data in plot_all_fields.ROME_FIELDS.items():
+
+            file_name = path.join(args.data_dir, field_id+'_colour.png')
+
+            if path.isfile(file_name):
+
+                image = plt.imread(file_name)
+                #image = np.fliplr(image)
+                image = np.flipud(image)
+
+                extent = [ field_data[0] - plot_all_fields.FIELD_HALF_WIDTH,
+                            field_data[0] + plot_all_fields.FIELD_HALF_WIDTH,
+                           field_data[1] - plot_all_fields.FIELD_HALF_HEIGHT,
+                            field_data[1] + plot_all_fields.FIELD_HALF_HEIGHT ]
+
+                plt.imshow(image, extent=extent)
+
+                # text = plt.text(field_data[0], field_data[1], field_id,
+                #                 fontdict={'fontsize': 14, 'fontweight': 'bold', 'color': 'white'},
+                #                           backgroundcolor='none')
+                # text.set_path_effects([withStroke(linewidth=3, foreground='black')])
+
+    ax.axis('equal')
+    #plot_ranges = plot_all_fields.calc_survey_boundaries()
+    #print(plot_ranges)
+    #ax.set_xlim([plot_ranges[1], plot_ranges[0]])
+    #ax.set_ylim([plot_ranges[2], plot_ranges[3]])
+    #plt.grid(color='white', ls='solid')
+    (xmin,xmax,ymin,ymax) = plt.axis()
+    plt.axis([xmax,xmin,ymin,ymax])
+    plt.grid(linestyle='--',color='gray', linewidth=0.5)
+    ax.tick_params(axis='x', colors='gray')
+    ax.tick_params(axis='y', colors='gray')
+    plt.xlabel('RA [deg]', fontsize=60)
+    plt.ylabel('Dec [deg]', fontsize=60)
+    ax.tick_params(labelsize=60, labelcolor='gray')
+    plt.tight_layout()
+    plt.savefig(path.join(args.output_dir, 'survey_map.png'))
+
+
+def create_wcs(ra_center, dec_center, dra, ddec, x_center, y_center):
 
     # Use the center of the image and Bulge region as the reference pixel
     crval = np.zeros(2)
-    crval[0] = BULGE_REGION['ra_range'][0] + (BULGE_REGION['ra_range'][1] - BULGE_REGION['ra_range'][0])/2.0
-    crval[1] = BULGE_REGION['dec_range'][0] + (BULGE_REGION['dec_range'][1] - BULGE_REGION['dec_range'][0])/2.0
+    crval[0] = ra_center
+    crval[1] = dec_center
 
     crpix = np.zeros(2)
-    crpix[0] = image_data.shape[1]/2.0
-    crpix[1] = image_data.shape[0]/2.0
+    crpix[0] = y_center
+    crpix[1] = x_center
 
     # Create the WCS object
     w = wcs.WCS(naxis=2)
     w.wcs.crpix = crpix
-    w.wcs.cdelt = np.array([RESOLUTION, RESOLUTION])
+    w.wcs.cdelt = np.array([dra, ddec])
     w.wcs.crval = crval
-    w.wcs.ctype = ["RA---TAN", "DEC--TAN"]
+    w.wcs.ctype = ["RA---AIT", "DEC--AIT"]
 
     return w
 
@@ -158,6 +235,7 @@ def get_args():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('star_map_file', help='Path to star density map file, e.g. /path/TRIstarDensity_r_nside_64.npz')
+    parser.add_argument('data_dir', help='Path to directory with ROME survey field images')
     parser.add_argument('output_dir', help='Path to output directory')
     args = parser.parse_args()
 
