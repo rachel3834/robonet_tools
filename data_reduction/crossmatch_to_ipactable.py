49877fcfe614f53c5ac89466a96d6ed155a07ca7
rachel3834 <rstreet@lco.global>
2024-07-11 09:51:46 -0700

    Debugging ndata columns

--- data_reduction/crossmatch_to_ipactable.py
+++ data_reduction/crossmatch_to_ipactable.py
@@ -190,6 +190,8 @@ def output_to_ipactable(args, source_table, log):
     for star in source_table:
         tbl_line = ' '
         for col, col_def in table_columns.items():
+            if 'ndata' in col:
+                print(col, star[col])
             if col == 'lc_file_path':
                 tbl_line += get_lc_file_path(args, star['field_id'])
             elif col in ['ra', 'dec']:
@@ -323,9 +325,9 @@ def extract_source_data(args, xmatch, variable_catalog, starcounts, log):
     column_list.append(Column(name='ndata_g', data=ndata_g))
     column_list.append(Column(name='ndata_r', data=ndata_r))
     column_list.append(Column(name='ndata_i', data=ndata_i))
-    print('NDATA_G:', ndata_g)
-    print('NDATA_G:', ndata_r)
-    print('NDATA_G:', ndata_i)
+    print('NDATA_G:', ndata_g.max())
+    print('NDATA_G:', ndata_r.max())
+    print('NDATA_G:', ndata_i.max())
 
     source_table = Table(column_list)
     log.info('Built source catalog table')
@@ -403,6 +405,10 @@ def extract_source_data(args, xmatch, variable_catalog, starcounts, log):
         #                 source_table['norm_mag_'+f][j] = xmatch.stars['norm_'+f+'_mag_'+site_code][j]
         #                 source_table['norm_mag_error_'+f][j] = xmatch.stars['norm_'+f+'_magerr_'+site_code][j]
     log.info('Populated source catalog table')
+    print('SOURCE TABLE NDATA:')
+    print(source_table['ndata_g'].max())
+    print(source_table['ndata_r'].max())
+    print(source_table['ndata_i'].max())
 
     return source_table
 
