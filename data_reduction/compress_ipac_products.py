f3384714518eaa6a766aadf59bb5a77a8f9e8b72
rachel3834 <rstreet@lco.global>
2024-11-11 20:47:31 -0800

    Added counter

--- data_reduction/compress_ipac_products.py
+++ data_reduction/compress_ipac_products.py
@@ -6,10 +6,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument('data_dir', help='Path to IPAC lightcurves top-level directory')
 args = parser.parse_args()
 
+nlc = 0
 for (root,dirs,files) in walk(args.data_dir):
     for f in files:
         if '.gz' not in f:
             file_path = path.join(root, f)
             result = subprocess.run(["gzip", file_path], capture_output=True, text=True)
-            print(result)
+            if '.fits' in f: nlc += 1
 
+print('Found ' + str(nlc) + ' lightcurves')
