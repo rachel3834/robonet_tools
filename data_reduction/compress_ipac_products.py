09e774771ff5559c59d484a4b4c57427dc1478bb
rachel3834 <rstreet@lco.global>
2024-11-11 20:06:02 -0800

    Added tool to compress IPAC-format products

---
+++ data_reduction/compress_ipac_products.py
@@ -0,0 +1,15 @@
+from os import path, walk
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument('data_dir', help='Path to IPAC lightcurves top-level directory')
+args = parser.parse_args()
+
+for (root,dirs,files) in walk(args.data_dir):
+    for f in files:
+        if '.gz' not in f:
+            file_path = path.join(root, f)
+            result = subprocess.run(["gzip", file_path], capture_output=True, text=True)
+            print(result)
+
